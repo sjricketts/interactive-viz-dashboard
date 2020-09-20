e9dd86e799af8b5c35e2458bf774849809fdf85e
Scott Ricketts <SJR@imac.lan>
2020-09-20 17:18:19 -0500

    Read in json data

---
+++ static/js/app.js
@@ -0,0 +1,31 @@
+
+// read .json data
+d3.json("static/samples.json").then((data) => {
+    console.log(data);
+    
+    // Sort descending
+
+    // Only take top 10
+    
+    
+    // Create bar chart trace
+    // let trace1 = {
+    //     x: ,
+    //     y: ,
+    //     type: 'bar',
+    //     orientation: 'h'
+    // };
+    // Use sample_values as the values for the bar chart.
+
+
+    // Use otu_ids as the labels for the bar chart.
+
+
+    // Use otu_labels as the hovertext for the chart.
+    // let traces = [trace1];
+    // let layout = {
+
+    // };
+
+    // Plotly.newPlot("bar", traces, layout);
+});

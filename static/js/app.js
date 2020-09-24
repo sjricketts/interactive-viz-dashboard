362812b50e349b7454fdde0a075f9dad6fbfbe26
Scott Ricketts <SJR@imac.lan>
2020-09-24 14:25:19 -0500

    Import and read json data

--- static/js/app.js
+++ static/js/app.js
@@ -1,31 +1,34 @@
 
 // read .json data
-d3.json("static/samples.json").then((data) => {
-    console.log(data);
-    
-    // Sort descending
+d3.json("samples.json").then((data) => {
+  var samples = data.samples;
+  var resultArray = samples.filter((sampleObj) => sampleObj.id == idx);
+  var result = resultArray[0];
+  console.log(result);
+  var sampleValues = result.sample_values;
+  var otuIds = result.otu_ids;
+  var otuLabels = result.otu_labels;
 
-    // Only take top 10
-    
-    
-    // Create bar chart trace
-    // let trace1 = {
-    //     x: ,
-    //     y: ,
-    //     type: 'bar',
-    //     orientation: 'h'
-    // };
-    // Use sample_values as the values for the bar chart.
+  // Sort descending
 
+  // Only take top 10
 
-    // Use otu_ids as the labels for the bar chart.
+  // Create bar chart trace
+  // let trace1 = {
+  //     x: ,
+  //     y: ,
+  //     type: 'bar',
+  //     orientation: 'h'
+  // };
+  // Use sample_values as the values for the bar chart.
 
+  // Use otu_ids as the labels for the bar chart.
 
-    // Use otu_labels as the hovertext for the chart.
-    // let traces = [trace1];
-    // let layout = {
+  // Use otu_labels as the hovertext for the chart.
+  // let traces = [trace1];
+  // let layout = {
 
-    // };
+  // };
 
-    // Plotly.newPlot("bar", traces, layout);
+  // Plotly.newPlot("bar", traces, layout);
 });
